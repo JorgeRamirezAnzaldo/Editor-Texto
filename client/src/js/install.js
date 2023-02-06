fddded68cf15ac23c5821fa09808a0067baead1e
Jorge Ramirez Anzaldo <jaanzaldo@hotmail.com>
2023-02-06 12:30:46 -0600

    Adding logic to install the PWA

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,33 @@
+//Get button install using its id
 const butInstall = document.getElementById('buttonInstall');
 
-// Lógica para instalar la PWA
-// TODO: Agregar un controlador de eventos al evento `beforeinstallprompt`
-window.addEventListener('beforeinstallprompt', (event) => {});
+//Implement logic to install the PWA
 
-// TODO: Implementar un controlador de eventos de clic en el elemento `butInstall`
-butInstall.addEventListener('click', async () => {});
+//Event controller for beforeinstallprompt event
+window.addEventListener('beforeinstallprompt', (event) => {
+    //Store active events
+    window.deferredPrompt = event;
+    //Set hidden class of the install button to false
+    butInstall.classList.toggle('hidden', false);
+});
 
-// TODO: Agregar un controlador para el evento `appinstalled`
-window.addEventListener('appinstalled', (event) => {});
+
+//Event controller for click event over butInstall element
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+    return;
+    }
+    //Show question
+    promptEvent.prompt();
+    //Reset variable for the deferredPrompt, it can only be used once
+    window.deferredPrompt = null;
+    //Set hidden class of the install button to true
+    butInstall.classList.toggle('hidden', true);
+});
+
+//Event controller for appinstalled event
+window.addEventListener('appinstalled', (event) => {
+    //Delete question
+    window.deferredPrompt = null;
+});
