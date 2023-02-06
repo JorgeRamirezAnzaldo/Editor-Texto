0a7bb3759d6c6ba50e1f11c68e687eeaa821ebf6
Jorge Ramirez Anzaldo <jaanzaldo@hotmail.com>
2023-02-06 11:33:26 -0600

    Adding webpack configuration to define a service worker and a manifest, along with the CSS and babel loaders

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,29 +1,78 @@
+//Import HTML Webpack Plugin
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+//Import Webpack PWA Manifest
 const WebpackPwaManifest = require('webpack-pwa-manifest');
+//Import path
 const path = require('path');
+//Import Inject Manifest from Workbox Webpack Plugin
 const { InjectManifest } = require('workbox-webpack-plugin');
-
-// TODO: Agregar y configurar complementos del workbox para un service worker y un archivo de manifiesto.
-// TODO: Agregar cargadores de CSS y babel al webpack.
 
 module.exports = () => {
   return {
     mode: 'development',
+    //Entry point for files
     entry: {
       main: './src/js/index.js',
       install: './src/js/install.js'
     },
+    //Output for packages
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      //Define template to generate html file with packages included
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: "JATE"
+      }),
+
+      //Inject our personalized service worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js'
+      }),
+
+      //Create a manifest.json file
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E.',
+        description: 'Takes notes with JavaScript syntax highlighting!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      })
     ],
 
     module: {
       rules: [
-        
+        //Add CSS loader to webpack
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          //Use babel loader to use ES6
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
