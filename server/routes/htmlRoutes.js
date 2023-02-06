2a898e72c49fb4ffa209e8b2a3ca38f5e0f5b667
Jorge Ramirez Anzaldo <jaanzaldo@hotmail.com>
2023-02-06 10:22:00 -0600

    Adding comments to server files

--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -1,6 +1,8 @@
+//Import path
 const path = require('path');
 
+//Export get method for app
 module.exports = (app) =>
   app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+    res.sendFile(path.join(__dirname, '../client/dist/index.html')) //Send index.html as response
   );
--- server/server.js
+++ server/server.js
@@ -1,12 +1,18 @@
+//Import express
 const express = require('express');
 
+//Initialize app
 const app = express();
+//Define port
 const PORT = process.env.PORT || 3000;
 
+//Configure app
 app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//Use htmlRoutes
 require('./routes/htmlRoutes')(app);
 
+//Make the app listen in the defined port
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
